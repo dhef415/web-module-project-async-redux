fa9fd4bc41bf42698223c931e013dc07a7ed1cc7
Daniel Hefner <danielhefner@daniels-mbp.gateway.ht.net>
2021-07-14 21:26:03 -1000

    search bar working

--- asyncreduxapp/src/App.js
+++ asyncreduxapp/src/App.js
@@ -1,23 +1,34 @@
 import { connect } from 'react-redux';
 import './App.css';
-
+import { useEffect } from 'react'
 import GifForm from './components/GifForm';
 import GifList from './components/GifList';
+import { getGifs } from './actions';
+
+import axios from 'axios';
+
 
 
 
 function App(props) {
-  console.log(props);
 
-  const { loading, err } = props;
 
-  if (err !== ''){
-    return <h3>{err}</h3>
-  }
+  const { loading, err,getGifs } = props;
+  
+  useEffect(()=>{
+    getGifs('dogs');
+    
+  },[]);
+
   return (
     <div className="App">
       <h1>Find A Gif</h1>
       <GifForm />
+
+      {
+        (err !== '') && <h3>{err}</h3>
+      }
+
 
 
       {
@@ -36,4 +47,12 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(App);
+// const mapActionsToProps = () => {
+//   return{
+//     fetchStart: fetchStart
+//   }
+// }
+
+
+
+export default connect(mapStateToProps, {getGifs})(App);
--- asyncreduxapp/src/reducer/index.js
+++ asyncreduxapp/src/reducer/index.js
@@ -1,13 +1,29 @@
 import data from './../data/tempdata';
 
+import { FETCH_START, FETCH_SUCCESS } from './../actions';
+
 const initialState = {
-    gifs: data,
+    gifs: [],
     loading: false,
     err: ''
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
+        case FETCH_START:
+            return{
+                ...state,
+                loading:true,
+                err:''
+            }
+        case FETCH_SUCCESS:
+            return{
+                ...state,
+                gifs:action.payload,
+                loading:false,
+                err:''
+                
+            }
         default:
             return(state);
     }
